5329ff3d97eee2df1d507bd9d45371977b214332
francescoditrani <f.ditrani@gmail.com>
2015-10-31 13:54:09 +0100

    fix

--- src/main/scala/org/leanpoker/player/Player.scala
+++ src/main/scala/org/leanpoker/player/Player.scala
@@ -64,7 +64,7 @@ object Player {
     }
   }
 
-  def areCoupleOfCardsIn(myCards: Seq[String]): Boolean = ???
+//  def areCoupleOfCardsIn(myCards: Seq[String]): Boolean = ???
 
 
   def calculateBetForPlayer(jsonObject: JsonObject, meAsAPlayer: JsonObject) = {
@@ -84,7 +84,7 @@ object Player {
     println(s"$largest_current $stack, $bet,$myCards, $playable, $call"  )
 
     myCards match {
-      case _ if areCoupleOfCardsIn(myCards) => raise
+//      case _ if areCoupleOfCardsIn(myCards) => raise
       case _ if aGoodCardIn(myCards) => call
       case _ => 0
     }
